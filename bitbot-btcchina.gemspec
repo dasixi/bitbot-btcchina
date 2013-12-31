ee548cf19595177d446f03991c51a670e73bf30f
tomlion <qycpublic@gmail.com>
2013-12-31 16:41:17 +0800

    Trim API response and shape it as an object

--- bitbot-btcchina.gemspec
+++ bitbot-btcchina.gemspec
@@ -4,9 +4,11 @@ Gem::Specification.new do |s|
   s.date        = '2013-12-30'
   s.summary     = "A bitbot adapter for btcchina"
   s.description = "A bitbot adapter for btcchina"
-  s.authors     = ["dasixi"]
+  s.authors     = ["dasixi", "tomlion"]
   s.email       = 'i@tomlion.com'
-  s.files       = ["lib/bitbot-btcchina.rb"]
   s.license       = 'GPL V2'
+  s.files       = `git ls-files`.split("\n")
+  s.homepage    = 'https://github.com/dasixi/bitbot-btcchina'
   s.add_dependency 'kublai'
+  s.add_dependency 'bitbot'
 end
--- lib/bitbot-btcchina.rb
+++ lib/bitbot-btcchina.rb
@@ -4,29 +4,43 @@ module BitBot
   module Btcchina
     ### INFO ###
     def ticker
-      client.ticker
+      map = {sell: :ask, buy: :bid}
+      Ticker.new rekey(client.ticker, map)
+    end
+
+    def offers
+      resp = client.get_market_depth
+      asks = resp['ask'].collect do |offer|
+        Offer.new offer
+      end
+      bids = resp['bid'].collect do |offer|
+        Offer.new offer
+      end
+      {asks: asks, bids: bids}
     end
 
     def asks
-      client.get_market_depth['ask']
+      offers[:asks]
     end
 
     def bids
-      client.get_market_depth['bid']
+      offers[:bids]
     end
 
     ### TRADES ###
-
     def buy(options)
       amount = options[:amount]
       price = options[:price]
       client.buy price, amount
+      orders.first
     end
 
+    # true
     def sell(options)
       amount = options[:amount]
       price = options[:price]
       client.sell price, amount
+      orders.first
     end
 
     def cancel(order_id)
@@ -35,11 +49,23 @@ module BitBot
 
     ### ACCOUNT ###
     def orders
-      client.get_orders
+      resp = client.get_orders['order']
+      resp.collect do |hash|
+        build_order(hash)
+      end
     end
 
 
     private
+    def build_order(hash)
+      map = {
+        amount: :remaining,
+        amount_original: :amount,
+        date: :timestamp
+      }
+      Order.new rekey(hash, map)
+    end
+
     def client
       @client ||= Kublai::BTCChina.new @key, @secret
     end
